b70975c1f1837292e8ae806db8796589298a1118
Dr. Matt Lee <mattl@cnuk.org>
2024-02-02 16:23:25 -0500

    Album prototype

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,19 @@
+class PrototypesController < ApplicationController
+  def index
+  end
+
+  def register
+  end
+
+  def login
+  end
+
+  def profile
+  end
+
+  def artist
+  end
+
+  def album
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "prototypes#index"
+  get "/prototypes", to: "prototypes#index"
+  get "/prototypes/artist", to: "prototypes#artist"
+  get "/prototypes/album", to: "prototypes#album"
+  get "/prototypes/login", to: "prototypes#login"
+  get "/prototypes/register", to: "prototypes#register"
+  get "/prototypes/profile", to: "prototypes#profile"
+
 end
