195ce88c9a68ae57ffe9563202d5fca4abec7f04
Dr. Matt Lee <mattl@cnuk.org>
2024-02-04 18:37:03 -0500

    user profiles, gravatar support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: %i[ show edit update destroy profile ]
 
   # GET /users or /users.json
   def index
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+def gravatar(user, opts = {})
+	size = opts[:size || 500]
+
+	  hash = Digest::MD5.hexdigest(user.email.downcase)
+	  "https://secure.gravatar.com/avatar/#{hash}.png?s=#{size}"
+ end
+
 end
